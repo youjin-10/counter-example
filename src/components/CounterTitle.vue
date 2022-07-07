0d8e768884b8e5b1e204cb677548b9c132ac3d36
youjin-fooding <yujhin1026@fooding.io>
2022-07-07 09:41:48 +0900

    Create CounterTitle.vue

---
+++ src/components/CounterTitle.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="text-gray-500 font-bold text-3xl">{{ counterNum }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+  props: {
+    counterNum: Number,
+  },
+});
+</script>
