ae31dadbe52840058ad0e1bc7566ebae3386bd07
youjin-fooding <yujhin1026@fooding.io>
2022-07-12 14:28:20 +0900

    add router

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import './index.css';
+import router from './router';
+import axios from 'axios';
+
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;
 
 const pinia = createPinia();
 
-createApp(App).use(pinia).mount('#app');
+createApp(App).use(pinia).use(router).mount('#app');
