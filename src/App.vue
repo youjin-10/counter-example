36e4f1bc798be079f488601538fa33a14b3a3a76
youjin-fooding <yujhin1026@fooding.io>
2022-07-12 14:26:51 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,13 @@
 <template>
-  <div class="max-w-md mx-auto text-center">
-    <CounterTitle :counterNum="counterStore.counterNumber" />
-    <div class="mt-5">
-      <PrimaryButton text="UP" @click-event="increaseNumber" />
-      <DefaultButton text="DOWN" @click-event="decreaseNumber" />
-    </div>
-  </div>
+  <router-view></router-view>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { useCounterStore } from './store/counter';
-import PrimaryButton from './components/Buttons/PrimaryButton.vue';
-import DefaultButton from './components/Buttons/DefaultButton.vue';
-import CounterTitle from './components/CounterTitle.vue';
 
 export default defineComponent({
   setup() {
-    const counterStore = useCounterStore();
-
-    return {
-      counterStore,
-    };
-  },
-  components: {
-    PrimaryButton,
-    DefaultButton,
-    CounterTitle,
-  },
-  methods: {
-    increaseNumber() {
-      this.counterStore.increment();
-    },
-    decreaseNumber() {
-      this.counterStore.decrement();
-    },
+    return {};
   },
 });
 </script>
