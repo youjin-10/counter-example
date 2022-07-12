815948e73820124dd33799a70b3585f10152ee8f
youjin-fooding <yujhin1026@fooding.io>
2022-07-12 14:28:05 +0900

    Create NotFoundPage.vue

---
+++ src/pages/notfound/NotFoundPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>이러지맙시다</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
