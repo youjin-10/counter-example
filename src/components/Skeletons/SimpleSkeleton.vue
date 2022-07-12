e9758bba71e47bea57eab2e7198e9aa9ae9175f2
youjin-fooding <yujhin1026@fooding.io>
2022-07-12 14:28:38 +0900

    Create SimpleSkeleton.vue

---
+++ src/components/Skeletons/SimpleSkeleton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="animate-pulse flex space-x-4">
+    <div class="flex-1 space-y-3 py-1">
+      <div class="h-2 bg-slate-200 rounded w-[350px]"></div>
+      <div class="h-2 bg-slate-200 rounded w-[350px]"></div>
+      <div class="h-2 bg-slate-200 rounded w-[350px]"></div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  setup() {
+    return {};
+  },
+});
+</script>
+
+<style scoped></style>
